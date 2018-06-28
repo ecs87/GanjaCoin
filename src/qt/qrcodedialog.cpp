9882cd6a94b0e75d36dd60a668014d52326f8190
Elson Smith <elsonsmith87@gmail.com>
2018-06-28 09:17:43 -0500

    URI functionality
    URI links cannot begin with a capital letter

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -83,7 +83,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("Ganjaproject:%1").arg(address);
+    QString ret = QString("ganjaproject:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
